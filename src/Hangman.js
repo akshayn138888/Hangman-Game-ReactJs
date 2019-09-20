6e09f07119094a4f82e03a1244d0bcdd492b215c
akshayn138888 <akshayn@sfu.ca>
2019-09-20 10:05:01 -0700

    added the game over feature, Guessed Wrong, you lose now aapearing

--- src/Hangman.js
+++ src/Hangman.js
@@ -11,7 +11,7 @@ import img6 from "./6.jpg";
 class Hangman extends Component {
   /** by default, allow 6 guesses and use provided gallows images. */
   static defaultProps = {
-    maxWrong: 6,
+    maxWrong:6,
     images: [img0, img1, img2, img3, img4, img5, img6]
   };
 
@@ -46,9 +46,11 @@ class Hangman extends Component {
   generateButtons() {
     return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
       <button
+        key={ltr}
         value={ltr}
         onClick={this.handleGuess}
         disabled={this.state.guessed.has(ltr)}
+        
       >
         {ltr}
       </button>
@@ -57,12 +59,16 @@ class Hangman extends Component {
 
   /** render: render game */
   render() {
+    let gameOver = this.state.nWrong>=this.props.maxWrong
     return (
       <div className='Hangman'>
         <h1>Hangman</h1>
         <img src={this.props.images[this.state.nWrong]} />
-        <p className='Hangman-word'>{this.guessedWord()}</p>
-        <p className='Hangman-btns'>{this.generateButtons()}</p>
+        <p>  Guessed Wrong : {this.state.nWrong}</p>
+        <p className='Hangman-word'>{!gameOver ? this.guessedWord(): this.state.answer}</p>
+        <p className='Hangman-btns'>
+        {!gameOver ? this.generateButtons() : `You Lose`}
+        </p>
       </div>
     );
   }
